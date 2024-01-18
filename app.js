aa1fb11e1b37d775cac0caeaa0307113e51d8b2b
DANIEL WILSON <daniel.j.wilson@btinternet.com>
2024-01-18 13:51:54 +0000

    setInterval now showing in browser window in score div

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 console.log("Welcome");
 
+let totalsmiles = document.getElementById("totalsmiles");
+
 let smiles = 0;
 setInterval(function () {
   smiles = smiles + 1;
+  totalsmiles.innerText = smiles;
   console.log("😁", smiles);
 }, 1000);

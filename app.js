502fba66666eb94989654ce283c6f4c62c1308ea
DANIEL WILSON <daniel.j.wilson@btinternet.com>
2024-01-18 21:04:17 +0000

    fixed hover animation

--- app.js
+++ app.js
@@ -1,33 +1,37 @@
 console.log("Welcome");
 
-const bonusPointTimer = 10000;
-const btn = document.getElementById("btn");
-const resetBtn = document.getElementById("resetBtn");
-const scoreElement = document.getElementById("totalsmiles");
+const bonusPointTimer = 10000; //setting timer duration in ms
+const btn = document.getElementById("btn"); //get reference to btn
+const resetBtn = document.getElementById("resetBtn"); //get reference to reset btn
+const scoreElement = document.getElementById("totalsmiles"); //get reference to element where score is displayed on html
 
-btn.addEventListener("click", incrementScore);
-resetBtn.addEventListener("click", resetScore);
+btn.addEventListener("click", incrementScore); //adding the listener for click event and the function it will call
+resetBtn.addEventListener("click", resetScore); //same as above but for reset button
 
-let score = Number(localStorage.getItem("myScore"));
-updateDisplay();
+let score = Number(localStorage.getItem("myScore")); //getting stored scored form local storage and covert to number
+updateDisplay(); //calling function to update screen
 
 setInterval(function () {
-  incrementScore();
+  //start timer to increment score at preset period set on line 3
+  incrementScore(); //increment score
 }, bonusPointTimer);
 
 function incrementScore() {
+  //function increments score by 1 and then updates display and saves to local storage
   score += 1;
   updateDisplay();
   localStorage.setItem("myScore", score);
 }
 
 function updateDisplay() {
+  //function to update display
   scoreElement.innerText = score;
-  console.log("😁", score);
+  console.log("😁", score); //writing to console log
 }
 
 function resetScore() {
+  //function to reset score back to 0 and updates display
   score = 0;
   updateDisplay();
-  localStorage.setItem("myScore", score);
+  localStorage.setItem("myScore", score); //saves to local storage
 }

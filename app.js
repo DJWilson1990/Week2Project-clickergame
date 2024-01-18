0696a213f402fe03a77aa679ff59fa611f103d1b
DANIEL WILSON <daniel.j.wilson@btinternet.com>
2024-01-18 14:58:41 +0000

    added reset button, changed setInterval to 5 seconds

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 console.log("Welcome");
 
+const btn = document.getElementById("btn");
+const rbtn = document.getElementById("rbtn");
 let totalsmiles = document.getElementById("totalsmiles");
 
 let smiles = 0;
@@ -7,9 +9,7 @@ setInterval(function () {
   smiles = smiles + 1;
   totalsmiles.innerText = smiles; //this line links smile count to browser div
   console.log("😁", smiles);
-}, 1000);
-
-const btn = document.getElementById("btn");
+}, 5000);
 
 btn.addEventListener("click", function () {
   smiles = smiles + 1;
@@ -20,4 +20,15 @@ let click = 0;
 function score() {
   click += 1;
   btn.innerText = click;
+}
+
+rbtn.addEventListener("click", function () {
+  smiles = 0;
+  totalsmiles.innerText = smiles;
+});
+
+let reset = 0;
+function score() {
+  click = 0;
+  rbtn.innerText = click;
 }

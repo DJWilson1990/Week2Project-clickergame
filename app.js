a3c0dfda68c8efc039eb3e0710857acfffa374b5
DANIEL WILSON <daniel.j.wilson@btinternet.com>
2024-01-18 14:41:11 +0000

    added 1 point per click

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ let totalsmiles = document.getElementById("totalsmiles");
 let smiles = 0;
 setInterval(function () {
   smiles = smiles + 1;
-  totalsmiles.innerText = smiles;
+  totalsmiles.innerText = smiles; //this line links smile count to browser div
   console.log("😁", smiles);
 }, 1000);
+
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", function () {
+  smiles = smiles + 1;
+  totalsmiles.innerText = smiles;
+});
+
+let click = 0;
+function score() {
+  click += 1;
+  btn.innerText = click;
+}

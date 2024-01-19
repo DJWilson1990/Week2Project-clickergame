fb4dd2c0813e79b573313494537a25348a04099e
DANIEL WILSON <daniel.j.wilson@btinternet.com>
2024-01-19 14:09:34 +0000

    started upgrades

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const bonusPointTimer = 10000; //setting timer duration in ms
 const btn = document.getElementById("btn"); //get reference to btn
 const resetBtn = document.getElementById("resetBtn"); //get reference to reset btn
 const scoreElement = document.getElementById("totalsmiles"); //get reference to element where score is displayed on html
+const buyBtn = document.getElementById("buyBtn"); ///////
+const upgradeCost = -10; ///////
+let scoreIncrease = 1; ///////
 
 btn.addEventListener("click", incrementScore); //adding the listener for click event and the function it will call
 resetBtn.addEventListener("click", resetScore); //same as above but for reset button
+buyBtn.addEventListener("click", decrementScore); ////////
 
 let score = Number(localStorage.getItem("myScore")); //getting stored score from local storage and covert to number
 updateDisplay(); //calling function to update screen
@@ -35,3 +39,27 @@ function resetScore() {
   updateDisplay();
   localStorage.setItem("myScore", score); //saves to local storage
 }
+
+function decrementScore() {
+  if (score >= 10) {
+    //////if score is equal to or bigger than 10, upgrade can be bought and 10 points deducted
+    score = score + upgradeCost;
+  } else {
+    ////// if not score will stay the same
+    score = score;
+  }
+
+  updateDisplay();
+  localStorage.setItem("myScore", score);
+}
+
+// function scoreIncrease() {
+//   score = incrementScore + scoreIncrease;
+// }
+//////////////////
+
+// function upgrade() {
+//   let scoreIncrease;
+//   incrementScore = score + scoreElement;
+// }
+////////////////
